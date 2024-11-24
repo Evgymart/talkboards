03d354a026d7781ce9441e67233758e2bfee28c1
Evgymart <alignant@yandex.ru>
2024-11-24 19:39:02 +0300

    phpunit install and run basic test

---
+++ tests/TestTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TalkBoards\Tests;
+
+use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\TestCase;
+
+#[CoversNothing]
+final class TestTest extends TestCase
+{
+    public function testTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
