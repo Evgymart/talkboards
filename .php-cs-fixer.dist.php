4f2a6441eeacbeffbfaf3c11480b1e179a50309c
Evgymart <alignant@yandex.ru>
2024-11-25 22:47:05 +0300

    pyh coding standard

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,12 +2,22 @@
 
 declare(strict_types=1);
 
-$finder = new PhpCsFixer\Finder()
-    ->in(__DIR__)
-    ->exclude('var');
-return new PhpCsFixer\Config()
-    ->setRules([
-        '@Symfony' => true,
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PHPyh\CodingStandard\PhpCsFixerCodingStandard;
 
-    ])
+$finder = new Finder()
+    ->in(__DIR__)
+    ->exclude('var')
+    ->append([
+        __FILE__,
+        __DIR__ . '/bin/console',
+    ]);
+
+$config = new Config()
+    ->setCacheFile(__DIR__ . '/var/.php-cs-fixer.cache')
     ->setFinder($finder);
+
+new PhpCsFixerCodingStandard()->applyTo($config);
+
+return $config;
