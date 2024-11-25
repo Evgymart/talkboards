7746075ab801e6de6ff1d631409e255b94330f6d
Evgymart <alignant@yandex.ru>
2024-11-25 23:25:20 +0300

    Configure rector

--- rector.php
+++ rector.php
@@ -3,14 +3,25 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\StringableForToStringRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Set\ValueObject\LevelSetList;
 
-return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/TalkBoards',
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->parallel();
+    $rectorConfig->cacheDirectory(__DIR__ . '/var/rector');
+    $rectorConfig->paths([
+        __DIR__ . '/bin/console',
         __DIR__ . '/config',
         __DIR__ . '/public',
+        __DIR__ . '/src',
         __DIR__ . '/tests',
-    ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
-    ->withTypeCoverageLevel(0);
+    ]);
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_83,
+    ]);
+    $rectorConfig->skip([
+        StringableForToStringRector::class,
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ]);
+};
